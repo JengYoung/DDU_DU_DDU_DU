46d3d86f9283633721151af85283b7e8c6e92da2
JengYoung <young_pallete@naver.com>
2023-01-20 14:56:57 +0900

    feat(component): completed basic todo-list
    - 사실 silent-refresh가 핵심이라, 약 5~10분간 투두리스트 핵심만 구현했다.

--- next/13-silent-refresh/src/pages/index.tsx
+++ next/13-silent-refresh/src/pages/index.tsx
@@ -1,7 +1,14 @@
 import { Inter } from '@next/font/google';
-import { useEffect, useRef } from 'react';
+import { FormEvent, useEffect, useRef, useState } from 'react';
 import { useUserAuthContext } from '../../context/UserAuth';
 import { useRouter } from 'next/router';
+import { v4 as uuidv4 } from 'uuid';
+
+interface TodoInterface {
+  id: string;
+  title: string;
+  completed: boolean;
+}
 
 export default function Home() {
   const router = useRouter();
@@ -14,6 +21,31 @@ export default function Home() {
   const nonce = 'abc';
 
   const { user, checked } = useUserAuthContext();
+
+  const [todos, setTodos] = useState<TodoInterface[]>([]);
+  const [inputValue, setInputValue] = useState('');
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const onSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    if (!inputValue) return;
+
+    setTodos((state) => [
+      ...state,
+      {
+        id: uuidv4(),
+        title: inputValue,
+        completed: false,
+      },
+    ]);
+
+    (inputRef.current as HTMLInputElement).value = '';
+    setInputValue(() => '');
+  };
+
+  const onToggle = (id: string) => {
+    setTodos((state) => state.map((v) => (v.id === id ? { ...v, completed: !v.completed } : v)));
+  };
 
   useEffect(() => {
     if (checked && !user.access_token) {
@@ -28,7 +60,30 @@ export default function Home() {
         ref={iframeRef}
         src={`/api/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&scope=${scope}&response_type=code&code_challenge=${code_challenge}&code_challenge_method=S256&state=${state}&nonce=${nonce}`}
       />
+
       <h2>TODOLIST 🙇🏻‍♂️</h2>
+
+      <form onSubmit={onSubmit}>
+        <input
+          ref={inputRef}
+          placeholder="입력"
+          onInput={(e) => setInputValue(() => (e.target as HTMLInputElement).value)}
+        />
+        <button type="submit">할 일 추가</button>
+      </form>
+
+      <ul>
+        {todos.map((v) => (
+          <li key={v.id}>
+            <p
+              style={{ textDecoration: v.completed ? 'line-through' : 'none' }}
+              onClick={() => onToggle(v.id)}
+            >
+              {v.title}
+            </p>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
