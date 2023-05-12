5b4eb2523fe6b8cc1dc74ffb48458a1df71c21eb
JengYoung <young_pallete@naver.com>
2023-05-12 23:58:48 +0900

    chore(config): set next configuration

---
+++ next/10/antd/4/next.config.js
@@ -0,0 +1,64 @@
+module.exports = withAntdLess({
+  experimental: {
+    scrollRestoration: true,
+  },
+  env: {},
+  onDemandEntries: {
+    maxInactiveAge: 1000 * 60 * 60,
+    pagesBufferLength: 5,
+  },
+  cssLoaderOptions: {},
+  lessVarsFilePath: './src/assets/variables.less',
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.node = { fs: 'empty' };
+    }
+
+    config.resolve = {
+      modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+      alias: {
+        '#': path.resolve(__dirname, 'src'),
+        '#assets': path.resolve(__dirname, 'src/assets/'),
+        '#data': path.resolve(__dirname, 'src/data/'),
+        '#apis': path.resolve(__dirname, 'src/apis/'),
+        '#store': path.resolve(__dirname, 'src/store/'),
+        '#utils': path.resolve(__dirname, 'src/utils/'),
+        '#pages': path.resolve(__dirname, 'src/pages/'),
+        '#hooks': path.resolve(__dirname, 'src/hooks/'),
+        '#components': path.resolve(__dirname, 'src/components/'),
+      },
+      extensions: ['', '.js', '.json', '.jsx'],
+      ...config.resolve,
+    };
+
+    config.module.rules.push({
+      test: /\.md$/,
+      use: 'raw-loader',
+    });
+
+    if (isServer) {
+      const antStyles = /antd\/.*?\/style.*?/;
+      const origExternals = [...config.externals];
+      config.externals = [
+        (context, request, callback) => {
+          if (request.match(antStyles)) {
+            return callback();
+          }
+          if (typeof origExternals[0] === 'function') {
+            origExternals[0](context, request, callback);
+          } else {
+            callback();
+          }
+        },
+        ...(typeof origExternals[0] === 'function' ? [] : origExternals),
+      ];
+
+      config.module.rules.unshift({
+        test: antStyles,
+        use: 'null-loader',
+      });
+    }
+
+    return config;
+  },
+});
--- next/10/antd/4/pages/index.js
+++
@@ -1,3 +0,0 @@
-export default function IndexPage() {
-  return <div>인덱스 페이지</div>;
-}
