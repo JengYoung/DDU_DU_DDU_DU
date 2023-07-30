a2f2fbfb63f3a2afe1bd74f424738b92ab09c10c
JengYoung <young_pallete@naver.com>
2023-07-30 16:24:22 +0900

    feat(component): inputs placeholder tests are passed

--- next/13-draggable/src/domains/auth/signup/Form.tsx
+++ next/13-draggable/src/domains/auth/signup/Form.tsx
@@ -4,7 +4,7 @@ import { Placeholders } from './constants'
 export const Ids = {
   "아이디입력": "form__user-id",
   "비밀번호입력": "form__user-pw",
-  "비밀번호입력확인": "form__user-pw-confirm"
+  "비밀번호확인": "form__user-pw-confirm"
 }
 export const SignUpForm = () => {
   return (
@@ -13,7 +13,16 @@ export const SignUpForm = () => {
         <span>아이디</span>
         <input id={Ids.아이디입력} placeholder={Placeholders.아이디입력} />
       </label>
-      <input placeholder={Placeholders.비밀번호입력} />
+
+      <label htmlFor={Ids.비밀번호입력}>
+        <span>비밀번호</span>
+        <input id={Ids.비밀번호입력} placeholder={Placeholders.비밀번호입력} />
+      </label>
+
+      <label htmlFor={Ids.비밀번호확인}>
+        <span>비밀번호 확인</span>
+        <input id={Ids.비밀번호확인} placeholder={Placeholders.비밀번호확인} />
+      </label>
     </div>
   )
 }
