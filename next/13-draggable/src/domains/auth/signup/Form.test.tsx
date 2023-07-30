2ca8ede45e2f989e2bcecb60b9f5efb2e2d1f241
JengYoung <young_pallete@naver.com>
2023-07-30 16:21:24 +0900

    test(component): password input placeholder is to be equal our Policy

--- next/13-draggable/src/domains/auth/signup/Form.test.tsx
+++ next/13-draggable/src/domains/auth/signup/Form.test.tsx
@@ -9,16 +9,26 @@ const renderForm = () => {
   render(<SignUpForm />)
 }
 
+const checkPlaceholderByLabelText = <T extends string>(labelText: string, placeholder: T) => () => {
+  const idInput: HTMLInputElement = screen.getByLabelText(labelText);
+  expect(idInput).toBeInTheDocument();
+  expect(idInput.placeholder).toEqual(placeholder)
+}
+
 test("Id placeholder is to be equal our Policy", () => {
   const given = new Given(renderForm)
-  
   const when = new When(noAction)
+  const then = new Then(checkPlaceholderByLabelText("아이디", Placeholders.아이디입력))
 
-  const then = new Then(() => {
-    const idInput: HTMLInputElement = screen.getByLabelText("아이디");
-    expect(idInput).toBeInTheDocument();
-    expect(idInput.placeholder).toEqual(Placeholders.아이디입력)
-  })
+  given.run();
+  when.run();
+  then.run();
+})
+
+test("Password placeholder is to be equal our Policy", () => {
+  const given = new Given(renderForm)
+  const when = new When(noAction)
+  const then = new Then(checkPlaceholderByLabelText("비밀번호", Placeholders.비밀번호입력))
 
   given.run();
   when.run();
